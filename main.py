80d4ae88273016291230a8add752657a8ca64b05
nchoriyev <nchoriyev43@gmail.com>
2024-08-02 03:43:57 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
+
+import requests
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6815654045:AAHUVeZcoCmZKOKQLEqoc5tlQEv1yV5lGOE'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("""Hi!\nI'm Spotifymusic!
+    /artist
+    /albom
+    /songs """)
+
+
+@dp.message_handler(commands=['songs'])
+async def send_songs(message: types.Message):
+    songs = requests.get(f'http://127.0.0.1:8000/song/')
+    for song in songs.json():
+        await message.reply(song)
+
+@dp.message_handler(commands=['artist'])
+async def send_songs(message: types.Message):
+    artists = requests.get(f'http://127.0.0.1:8000/artist/')
+    for artist in artists.json():
+        await message.reply(artist)
+
+
+@dp.message_handler(commands=['albom'])
+async def send_songs(message: types.Message):
+    alboms = requests.get(f'http://127.0.0.1:8000/albom/')
+    for albom in alboms.json():
+        await message.reply(albom)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
